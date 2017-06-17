8f35a849f905f01613110239c0890470d102b68e
sat <sat@sandiego.edu>
2017-06-16 17:33:33 -0700

    can now use byte, word, and quad instructions

--- src/reversey/backy/x86Instruction.java
+++ src/reversey/backy/x86Instruction.java
@@ -60,10 +60,18 @@ public abstract class x86Instruction {
 		InstructionType type;
 		OpSize size;
 
-		String validInstrNames = "^(add|sub|xor|or|and|shl|sal|shr|sar|mov|lea|inc|dec|neg|not|push|pop)(l|q)$";
+		String validInstrNames = "^(add|sub|xor|or|and|shl|sal|shr|sar|mov|lea|inc|dec|neg|not|push|pop)(b|w|l|q)$";
 		if (Pattern.matches(validInstrNames, instrName)) {
 			type = InstructionType.valueOf(instrName.substring(0, instrName.length()-1).toUpperCase());
 			switch (instrName.charAt(instrName.length()-1)) {
+				case 'b':
+					System.out.println("instruction size is BYTE");
+					size = OpSize.BYTE;
+					break;
+				case 'w':
+					System.out.println("instruction size is WORD");
+					size = OpSize.WORD;
+					break;
 				case 'l':
 					System.out.println("instruction size is LONG");
 					size = OpSize.LONG;
@@ -98,20 +106,21 @@ public abstract class x86Instruction {
 		String longRegNames = "^\\%(e(ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)d)$";
 		String quadRegNames = "^\\%r(ax|bx|cx|dx|si|di|bp|sp|8|9|10|11|12|13|14|15)$";
 		String wordRegNames = "^\\%((ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)w)$";
-		String byteRegNames = "^\\%((al|ah|bl|bh|cl|ch|dl|dh|sil|dil|bpl|spl)|r(8|9|10|11|12|13|14|15)w)$";
+		String byteRegNames = "^\\%((al|ah|bl|bh|cl|ch|dl|dh|sil|dil|bpl|spl)|r(8|9|10|11|12|13|14|15)b)$";
 		if (Pattern.matches(longRegNames, str)) {
 			opSize = OpSize.LONG;
 		}
 		else if (Pattern.matches(quadRegNames, str)) {
 			opSize = OpSize.QUAD;
 		}
-		else if (Pattern.matches(wordRegNames, str) || Pattern.matches(wordRegNames, str)) {
-			// TODO: BYTE and WORD registers
-			System.err.println("ERROR: only LONG and QUAD registers currently supported");
-			return null; // TODO: throw exception
+		else if (Pattern.matches(wordRegNames, str)) {
+			opSize = OpSize.WORD;
+		}
+		else if (Pattern.matches(byteRegNames, str)) {
+			opSize = OpSize.BYTE;
 		}
 		else {
-			System.err.println("ERROR: unknown instruction name");
+			System.err.println("ERROR: unknown register name");
 			return null; // TODO: throw exception
 		}
 
@@ -391,12 +400,11 @@ class x86UnaryInstruction extends x86Instruction {
 				this.operation = 
 					(state, src) -> { 
 						// step 1: subtract 8 from rsp
-						// FIXME: esp should become rsp, 4 should become 8
-						RegOperand rsp = new RegOperand("esp", OpSize.LONG);
-						MachineState tmp = rsp.updateState(state, rsp.getValue(state).subtract(BigInteger.valueOf(4)), flags);
+						RegOperand rsp = new RegOperand("rsp", OpSize.QUAD);
+						MachineState tmp = rsp.updateState(state, rsp.getValue(state).subtract(BigInteger.valueOf(8)), flags);
 
 						// step 2: store src operand value in (%rsp)
-						MemoryOperand dest = new MemoryOperand("esp", null, 1, 0, OpSize.LONG);
+						MemoryOperand dest = new MemoryOperand("rsp", null, 1, 0, this.opSize);
 						return dest.updateState(tmp, src.getValue(tmp), flags); 
 					};
 				break;
@@ -405,13 +413,12 @@ class x86UnaryInstruction extends x86Instruction {
 				this.operation = 
 					(state, dest) -> { 
 						// step 1: store (%rsp) value in dest operand 
-						MemoryOperand src = new MemoryOperand("esp", null, 1, 0, OpSize.LONG);
+						MemoryOperand src = new MemoryOperand("rsp", null, 1, 0, this.opSize);
 						MachineState tmp = dest.updateState(state, src.getValue(state), flags); 
 
 						// step 2: add 8 to rsp
-						// FIXME: esp should become rsp, 4 should become 8
-						RegOperand rsp = new RegOperand("esp", OpSize.LONG);
-						return rsp.updateState(tmp, rsp.getValue(tmp).add(BigInteger.valueOf(4)), flags);
+						RegOperand rsp = new RegOperand("rsp", OpSize.QUAD);
+						return rsp.updateState(tmp, rsp.getValue(tmp).add(BigInteger.valueOf(8)), flags);
 
 					};
 				break;
@@ -896,10 +903,10 @@ class MemoryOperand extends Operand {
 	 * @param state The state in which to calculate the address.
 	 * @return The effective address.
 	 */
-	public int calculateAddress(MachineState state) { // FIXME: should this return long?
-		int address = state.getRegisterValue(baseReg).add(BigInteger.valueOf(offset)).intValue();
+	public long calculateAddress(MachineState state) { /** @tricky should this return BigInteger */
+		long address = state.getRegisterValue(baseReg).add(BigInteger.valueOf(offset)).longValue();
 		if (indexReg != null) {
-			address += state.getRegisterValue(indexReg).multiply(BigInteger.valueOf(scale)).intValue();
+			address += state.getRegisterValue(indexReg).multiply(BigInteger.valueOf(scale)).longValue();
 		}
 
 		return address;
@@ -973,7 +980,7 @@ class MachineState {
 	/**
 	 * The machine's memory.
 	 */
-	private Map<Integer, Byte> memory;
+	private Map<Long, Byte> memory;
 
 	/**
 	 * The status flags (i.e. condition codes).
@@ -986,21 +993,25 @@ class MachineState {
 	 */
 	public MachineState() {
 		this.registers = new HashMap<String, byte[]>();
-		this.memory = new HashMap<Integer, Byte>();
+		this.memory = new HashMap<Long, Byte>();
 		this.statusFlags = new HashMap<String, Boolean>();
 
-		String[] regNames = {"eax", "ebx", "ecx", "edx", "esi", "edi", "ebp", "r8d", "r9d", "r10d", "r11d", "r12d", "r13d", "r14d", "r15d"};
+		String[] regNames = {"rax", "rbx", "rcx", "rdx", "rsi", "rdi", "rbp", "r8", "r9", "r10", "r11", "r12", "r13", "r14", "r15"};
 		for (String s : regNames)
-			registers.put(s, new byte[4]); // FIXME: should be 8 to support 64-bit registers
+			registers.put(s, new byte[8]);
 
-		registers.put("esp", ByteBuffer.allocate(4).putInt(0x7FFFFFFF).array()); // FIXME 8 bytes when adding 64-bit support
+		long initRSP = 1 << 30;
+		initRSP <<= 30;
+		initRSP <<= 3;
+		initRSP = ~initRSP;
+		registers.put("rsp", ByteBuffer.allocate(8).putLong(initRSP).array()); // rsp = 0x7FFFFFFFFFFFFFFF
 
 		String[] flagNames = {"zf", "sf", "of", "cf"};
 		for (String s : flagNames)
 			statusFlags.put(s, false);
 	}
 
-	public MachineState(Map<String, byte[]> reg, Map<Integer, Byte> mem, Map<String, Boolean> flags) {
+	public MachineState(Map<String, byte[]> reg, Map<Long, Byte> mem, Map<String, Boolean> flags) {
 		this.registers = reg;
 		this.memory = mem;
 		this.statusFlags = flags;
@@ -1017,17 +1028,18 @@ class MachineState {
 	 * @return A new state that is the same as the current but with new binding
 	 * from given address to given val.
 	 */
-	public MachineState getNewState(int address, BigInteger val, int size, Map<String, Boolean> flags) {
-		Map<Integer, Byte> mem = new HashMap<Integer, Byte>(this.memory);
+	public MachineState getNewState(long address, BigInteger val, int size, Map<String, Boolean> flags) {
+		Map<Long, Byte> mem = new HashMap<Long, Byte>(this.memory);
 
 		byte[] valArray = val.toByteArray();
 
-		for (int src = 0, dest = address + (valArray.length - 1);
+		long dest = address + (valArray.length - 1);
+		for (int src = 0;
 				src < valArray.length; src++, dest++) {
 			mem.put(dest, valArray[src]);
 		}
 
-		for (int i = address + valArray.length; i < (address + size); i++) {
+		for (long i = address + valArray.length; i < (address + size); i++) {
 			if (val.signum() == -1) mem.put(i, (byte)0xFF);
 			else mem.put(i, (byte)0);
 		}
@@ -1043,6 +1055,78 @@ class MachineState {
 	}
 
 	/**
+	 * @return Pair of the start (inclusive) and end (exclusive) indices for
+	 * given register in its full register's byte array.
+	 */
+	private static Pair<Integer, Integer> getByteRange(String regName) {
+		String quadRegNames = "^r(ax|bx|cx|dx|si|di|bp|sp|8|9|10|11|12|13|14|15)$";
+		String longRegNames = "^(e(ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)d)$";
+		String wordRegNames = "^((ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)w)$";
+		String byteLowRegNames = "^((al|bl|cl|dl|sil|dil|bpl|spl)|r(8|9|10|11|12|13|14|15)b)$";
+		String byteHighRegNames = "^(ah|bh|ch|dh)$";
+		if (Pattern.matches(quadRegNames, regName)) {
+			return new Pair<Integer, Integer>(0, 8);
+		}
+		else if (Pattern.matches(longRegNames, regName)) {
+			return new Pair<Integer, Integer>(4, 8);
+		}
+		else if (Pattern.matches(wordRegNames, regName)) {
+			return new Pair<Integer, Integer>(6, 8);
+		}
+		else if (Pattern.matches(byteLowRegNames, regName)) {
+			return new Pair<Integer, Integer>(7, 8);
+		}
+		else if (Pattern.matches(byteHighRegNames, regName)) {
+			return new Pair<Integer, Integer>(6, 7);
+		}
+		else {
+			System.err.println("ERROR: Unknown register name: " + regName);
+			System.exit(1);
+			return null;
+		}
+	}
+
+	private static String getQuadName(String regName) {
+		String quadRegNames = "^r(ax|bx|cx|dx|si|di|bp|sp|8|9|10|11|12|13|14|15)$";
+		String longRegNames = "^(e(ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)d)$";
+		String wordRegNames = "^((ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)w)$";
+		String byteLowRegNames = "^((al|bl|cl|dl|sil|dil|bpl|spl)|r(8|9|10|11|12|13|14|15)b)$";
+		String byteHighRegNames = "^(ah|bh|ch|dh)$";
+		if (Pattern.matches(quadRegNames, regName)) {
+			return regName;
+		}
+		else if (Pattern.matches(longRegNames, regName)) {
+			if (regName.charAt(0) == 'e')
+				return "r" + regName.substring(1);
+			else
+				return regName.substring(0, regName.length()-1);
+		}
+		else if (Pattern.matches(wordRegNames, regName)) {
+			if (regName.charAt(0) != 'r')
+				return "r" + regName;
+			else
+				// just strip off the "d" from the end
+				return regName.substring(0, regName.length()-1);
+		}
+		else if (Pattern.matches(byteLowRegNames, regName)) {
+			if (regName.charAt(0) == 'r')
+				return regName.substring(0, regName.length()-1);
+			else if (regName.length() == 2)
+				return "r" + regName.substring(0, regName.length()-1) + "x";
+			else
+				return "r" + regName.substring(0, regName.length()-1);
+		}
+		else if (Pattern.matches(byteHighRegNames, regName)) {
+			return "r" + regName.substring(0, regName.length()-1) + "x";
+		}
+		else {
+			System.err.println("ERROR: Unknown register name: " + regName);
+			System.exit(1);
+			return null;
+		}
+	}
+
+	/**
 	 * Create a new MachineState based on the current state but with an updated
 	 * value for a register.
 	 *
@@ -1053,9 +1137,14 @@ class MachineState {
 	 * from given register to given val
 	 */
 	public MachineState getNewState(String regName, BigInteger val, Map<String, Boolean> flags) {
-		Map<String, byte[]> reg = new HashMap<String, byte[]>(registers);
+		String quadName = getQuadName(regName);
+		Pair<Integer, Integer> range = getByteRange(regName);
+		int startIndex = range.getKey();
+		int endIndex = range.getValue();
+
+		Map<String, byte[]> reg = new HashMap<String, byte[]>(this.registers);
 		byte[] valArray = val.toByteArray();
-		byte[] newVal = new byte[4]; // FIXME: this should be 8 for x86-64
+		byte[] newVal = new byte[endIndex-startIndex];
 
 		for (int src = 0, dest = (newVal.length - valArray.length); 
 				src < valArray.length; src++, dest++) {
@@ -1067,7 +1156,11 @@ class MachineState {
 				newVal[i] = (byte)0xFF;
 		}
 
-		reg.put(regName, newVal);
+		byte[] newValFull = Arrays.copyOf(this.registers.get(quadName), 8);
+		for (int src = 0, dest = startIndex; dest < endIndex; src++, dest++)
+			newValFull[dest] = newVal[src];
+
+		reg.put(quadName, newValFull);
 
 		// TODO: remove code duplication (here and in other version of
 		// getNewState.
@@ -1083,7 +1176,39 @@ class MachineState {
 	 * Gets the value stored in the given register.
 	 */
 	public BigInteger getRegisterValue(String regName) {
-		return new BigInteger(registers.get(regName));
+		byte[] ba = null;
+
+		String quadName = getQuadName(regName);
+		Pair<Integer, Integer> range = getByteRange(regName);
+		int startIndex = range.getKey();
+		int endIndex = range.getValue();
+
+		ba = registers.get(quadName);
+
+		/*
+		String longRegNames = "^(e(ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)d)$";
+		String quadRegNames = "^r(ax|bx|cx|dx|si|di|bp|sp|8|9|10|11|12|13|14|15)$";
+		String wordRegNames = "^((ax|bx|cx|dx|si|di|bp|sp)|r(8|9|10|11|12|13|14|15)w)$";
+		String byteRegNames = "^((al|ah|bl|bh|cl|ch|dl|dh|sil|dil|bpl|spl)|r(8|9|10|11|12|13|14|15)b)$";
+		if (Pattern.matches(quadRegNames, regName)) {
+			ba = registers.get(regName);
+		}
+		else if (Pattern.matches(longRegNames, regName)) {
+			startIndex = 4;
+
+			if (regName.charAt(0) == 'e')
+				ba = registers.get("r" + regName.substring(1));
+			else
+				ba = registers.get(regName.substring(0, regName.length()-1));
+		}
+		else {
+			System.err.println("ERROR: WORD AND BYTE registers not yet supported.");
+			System.exit(1);
+		}
+		*/
+
+		return new BigInteger(Arrays.copyOfRange(ba, startIndex, endIndex));
+		//return new BigInteger(registers.get(regName));
 	}
 
 	/**
@@ -1092,10 +1217,11 @@ class MachineState {
 	 * @param address The starting address where the value is stored.
 	 * @param size The number of bytes of memory to read.
 	 */
-	public BigInteger getMemoryValue(int address, int size) {
-		byte[] val = new byte[4];
+	public BigInteger getMemoryValue(long address, int size) {
+		byte[] val = new byte[size];
 
-		for (int addr = address+(size-1), dest = 0; dest < size; addr--, dest++)
+		long addr = address+(size-1);
+		for (int dest = 0; dest < size; addr--, dest++)
 			val[dest] = memory.get(addr);
 
 		return new BigInteger(val);
@@ -1106,7 +1232,6 @@ class MachineState {
 		for (Map.Entry<String, byte[]> entry : registers.entrySet()) {
 			BigInteger b = new BigInteger(entry.getValue());
 			byte[] ba = b.toByteArray();
-			//s += "\t" + entry.getKey() + ": " + b.toString() + " (0x" + b.toString(16) + ")\n";
 			s += "\t" + entry.getKey() + ": " + b.toString() + " (0x";
 			for (byte i : ba)
 				s += String.format("%02x", i);
@@ -1119,8 +1244,8 @@ class MachineState {
 		}
 
 		s += "Memory:\n";
-		for (Map.Entry<Integer, Byte> entry : memory.entrySet()) {
-			s += "\t" + Integer.toHexString(entry.getKey()) + ": " + String.format("%02x", entry.getValue()) + "\n";
+		for (Map.Entry<Long, Byte> entry : memory.entrySet()) {
+			s += "\t" + Long.toHexString(entry.getKey()) + ": " + String.format("%02x", entry.getValue()) + "\n";
 		}
 
 		return s;
@@ -1145,17 +1270,21 @@ interface UnaryX86Operation {
 class x86InstructionTester {
 	public static void main(String[] args) {
 		ArrayList<x86Instruction> instructions = new ArrayList<x86Instruction>();
-		instructions.add(x86Instruction.parseInstruction("movl $9, %eax"));
-		instructions.add(x86Instruction.parseInstruction("movl $4, %ebx"));
-		instructions.add(x86Instruction.parseInstruction("addl %eax, %ebx"));
-		instructions.add(x86Instruction.parseInstruction("pushl %ebx"));
-		instructions.add(x86Instruction.parseInstruction("popl %ecx"));
-		instructions.add(x86Instruction.parseInstruction("leal -8(%esp), %edx"));
-		instructions.add(x86Instruction.parseInstruction("movl $73, (%edx)"));
+		instructions.add(x86Instruction.parseInstruction("movq $9, %rax"));
+		instructions.add(x86Instruction.parseInstruction("movq $4, %rbx"));
+		instructions.add(x86Instruction.parseInstruction("addq %rax, %rbx"));
+		instructions.add(x86Instruction.parseInstruction("pushq %rbx"));
+		instructions.add(x86Instruction.parseInstruction("popq %rcx"));
+		instructions.add(x86Instruction.parseInstruction("leaq -12(%rsp), %rdx"));
+		instructions.add(x86Instruction.parseInstruction("movl $73, (%rdx)"));
 		instructions.add(x86Instruction.parseInstruction("incl %esi"));
 		instructions.add(x86Instruction.parseInstruction("decl %edi"));
 
-		// TODO: some instructions aren't tested (e.g. most shifts)
+		// test that smaller register only affect part of the whole register
+		instructions.add(x86Instruction.parseInstruction("movl $0, %edx"));
+		instructions.add(x86Instruction.parseInstruction("movw $-1, %dx"));
+		instructions.add(x86Instruction.parseInstruction("movb $2, %dl"));
+		instructions.add(x86Instruction.parseInstruction("movb $3, %dh"));
 
 		// tests for condition codes
 		instructions.add(x86Instruction.parseInstruction("movl $0, %ebp"));
