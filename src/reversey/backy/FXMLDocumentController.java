9c6055ec9dfbfdc17df478ceb66ef87ad9f1e98e
Caitlin <Caitlin@172.17.65.207>
2017-06-21 11:55:32 -0700

    Laid foundation for most prominent ordering of registers

--- src/reversey/backy/FXMLDocumentController.java
+++ src/reversey/backy/FXMLDocumentController.java
@@ -22,6 +22,8 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 
 /**
  *
@@ -29,134 +31,194 @@ import javafx.scene.image.ImageView;
  */
 public class FXMLDocumentController implements Initializable {
 
-	private MachineState currState;
+    private MachineState currState;
 
-    @FXML private TextField instrText;
-    @FXML private ListView<x86Instruction> instrList;
-    @FXML private MenuButton insertMenu;
-    @FXML private Button nextInstr;
-    @FXML private Button skipToEnd;
-    @FXML private Button prevInstr;
-    @FXML private Button skipToStart;
-    @FXML private Button currInstr;
-    @FXML private TableView stackTable;
-    @FXML private TableView promRegTable;
-    @FXML private TableColumn stackAddress;
-    @FXML private TableColumn stackVal;
-    @FXML private TableColumn stackOrigin;
-    @FXML private TableColumn registerName;
-    @FXML private TableColumn registerVal;
-    ObservableList<String> registerTableList;
-    
+    @FXML
+    private TextField instrText;
+    @FXML
+    private ListView<x86Instruction> instrList;
+    @FXML
+    private MenuButton insertMenu;
+    @FXML
+    private HBox buttonHBox;
+    @FXML
+    private Button nextInstr;
+    @FXML
+    private Button skipToEnd;
+    @FXML
+    private Button prevInstr;
+    @FXML
+    private Button skipToStart;
+    @FXML
+    private Button currInstr;
+    @FXML
+    private TableView stackTable;
+    @FXML
+    private TableView promRegTable;
+    @FXML
+    private TableColumn stackAddress;
+    @FXML
+    private TableColumn stackVal;
+    @FXML
+    private TableColumn stackOrigin;
+    @FXML
+    private TableColumn registerName;
+    @FXML
+    private TableColumn registerVal;
+    @FXML
+    private GridPane entireWindow;
+    ObservableList<Register> registerTableList;
+
     @Override
     public void initialize(URL foo, ResourceBundle bar) {
 
         currState = new MachineState();
-        
-        List list = new ArrayList();
 
+        nextInstr.setOnAction((event) -> {
+            System.out.println(instrList.getSelectionModel().getSelectedItem());
+            this.currState = instrList.getSelectionModel().getSelectedItem().eval(this.currState);
+            System.out.println(currState);
+            
+            // get used registers for next up instruction
+            // (future) get used registers for previous two instructions
+            // get registers for current state (as a list)
+            // sort list with most prominent at beginning
+            
+            registerTableList = FXCollections.observableArrayList(currState.getRegisters());
+            promRegTable.setItems(registerTableList);
+            instrList.getSelectionModel().selectNext();
+        });
+
+        //TODO: Keep history list of previous registers HashMap
+        skipToEnd.setOnAction((event) -> {
+            System.out.println(instrList.getSelectionModel().getSelectedItem());
+            instrList.getSelectionModel().selectLast();
+        });
+
+        prevInstr.setOnAction((event) -> {
+            System.out.println(instrList.getSelectionModel().getSelectedItem());
+            instrList.getSelectionModel().selectPrevious();
+        });
+
+        skipToStart.setOnAction((event) -> {
+            System.out.println(instrList.getSelectionModel().getSelectedItem());
+            instrList.getSelectionModel().selectFirst();
+        });
+
+        //“Give a man a program, frustrate him for a day. 
+        //Teach a man to program, frustrate him for a lifetime.” 
+        instrText.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                if (keyEvent.getCode() == KeyCode.ENTER) {
+                    String text = instrText.getText();
+
+                    x86Instruction x = x86Instruction.parseInstruction(text);
+
+                    //Enter text in listView
+                    instrList.getItems().addAll(x);
+                    instrText.clear();
+                }
+            }
+        });
+
+        //TODO: if instruction is valid, do the thingy
+        //stackPane.setOnKeyPressed(new EventHandler<KeyEvent>);
+        //TODO: if the instruction uses registers, display the registers in regTable
+        /*List list = new ArrayList();
+        
         list.add(new Register("%rax", "1"));
         list.add(new Register("%rbx", "2"));
         list.add(new Register("%rcx", "3"));
         list.add(new Register("%rdx", "4"));
-
-        registerTableList = FXCollections.observableArrayList(list);
+         */
+        registerTableList = FXCollections.observableArrayList(currState.getRegisters());
         promRegTable.setItems(registerTableList);
-        
+
         registerName.setCellValueFactory(new PropertyValueFactory("name"));
         registerVal.setCellValueFactory(new PropertyValueFactory("value"));
 
-         nextInstr.setOnAction((event) -> {
-             System.out.println(instrList.getSelectionModel().getSelectedItem());
-             this.currState = instrList.getSelectionModel().getSelectedItem().eval(this.currState);
-             System.out.println(currState);
-             instrList.getSelectionModel().selectNext();
-         });
-        
-       //TODO: Keep history list of previous registers HashMap
-         skipToEnd.setOnAction((event) -> {
-             System.out.println(instrList.getSelectionModel().getSelectedItem());
-             instrList.getSelectionModel().selectLast();
-         });
-         
-         prevInstr.setOnAction((event) -> {
-             System.out.println(instrList.getSelectionModel().getSelectedItem());
-             instrList.getSelectionModel().selectPrevious();
-         });
-         
-         skipToStart.setOnAction((event) -> {
-             System.out.println(instrList.getSelectionModel().getSelectedItem());
-             instrList.getSelectionModel().selectFirst();
-         });
-         
-    //“Give a man a program, frustrate him for a day. 
-    //Teach a man to program, frustrate him for a lifetime.” 
-    instrText.setOnKeyPressed(new EventHandler<KeyEvent>() {
-           @Override
-           public void handle(KeyEvent keyEvent){
-                if (keyEvent.getCode() == KeyCode.ENTER){
-                       String text = instrText.getText();
-                       
-                       x86Instruction x = x86Instruction.parseInstruction(text);
-                       
-                       //Enter text in listView
-                       instrList.getItems().addAll(x);
-                       //Clear text from instruction pane
-                       instrText.clear();
-                } 
-    }
-    });
-            
-    //TODO: if instruction is valid, do the thingy
-    //stackPane.setOnKeyPressed(new EventHandler<KeyEvent>);
-    
-    //TODO: if the instruction uses registers, display the registers in regTable
-    
-    //TODO: if user wants to change where the instruction should be inserted
-    MenuItem beginning = insertMenu.getItems().get(0);  
-    MenuItem current = insertMenu.getItems().get(1);
-    //MenuItem current
-    beginning.setText("At beginning");
-    current.setText("At current");
-    
-    Image skipStartImg = new Image(getClass().getResourceAsStream("skipToStart.png"));
-    Image prevInstrImg = new Image(getClass().getResourceAsStream("prevInstr.png"));
-    Image currInstrImg = new Image(getClass().getResourceAsStream("currInstr.jpg"));
-    Image nextInstrImg = new Image(getClass().getResourceAsStream("nextInstr.png"));
-    Image skipEndImg = new Image(getClass().getResourceAsStream("Sharonisgenius.png"));
-   
-    //Fix size 
-    /*
-    skipToStart.setGraphic(new ImageView(skipStartImg));
-    prevInstr.setGraphic(new ImageView(prevInstrImg));
-    //currInstr.setGraphic(new ImageView(currInstrImg));
-    nextInstr.setGraphic(new ImageView(nextInstrImg));
-    skipToEnd.setGraphic(new ImageView(skipEndImg));
-    */
-    
-    beginning.setOnAction((event) -> {
-        System.out.println("Option 1 selected");
-    });
-    
-    current.setOnAction((event) -> {
-           System.out.println("Option 2 selected");
-    });
+        //TODO: if user wants to change where the instruction should be inserted
+        MenuItem beginning = insertMenu.getItems().get(0);
+        MenuItem current = insertMenu.getItems().get(1);
+        //MenuItem current
+        beginning.setText("At beginning");
+        current.setText("At current");
 
-    
-    //Highlighting selected instruction is newly added item has an index of N
-    //instrList.getSelectionModel().getSelectedItem();
-    //instrList.getFocusModel().focus(N);
-    //Highlighting scrolled then selected
-    //instrList.scrollTo(N);
-    
-	Platform.runLater(new Runnable() {
+        Image skipStartImg = new Image(getClass().getResourceAsStream("skipToStart.png"));
+        Image prevInstrImg = new Image(getClass().getResourceAsStream("prevInstr.png"));
+        Image currInstrImg = new Image(getClass().getResourceAsStream("currInstr.png"));
+        Image nextInstrImg = new Image(getClass().getResourceAsStream("nextInstr.png"));
+        Image skipEndImg = new Image(getClass().getResourceAsStream("skipToEnd.png"));
 
-		@Override
-		public void run(){
-			// instrList.scrollTo(N);
-			// instrList.getSelectionModel().select(N);
-		}
-	});
-         
+        ImageView skipToStartImgVw = new ImageView(skipStartImg);
+        ImageView prevInstrImgVw = new ImageView(prevInstrImg);
+        ImageView currInstrImgVw = new ImageView(currInstrImg);
+        ImageView nextInstrImgVw = new ImageView(nextInstrImg);
+        ImageView skipToEndImgVw = new ImageView(skipEndImg);
+
+        skipToStartImgVw.setFitHeight(35);
+        skipToStartImgVw.setFitWidth(35);
+        prevInstrImgVw.setFitHeight(35);
+        prevInstrImgVw.setFitWidth(35);
+        currInstrImgVw.setFitHeight(35);
+        currInstrImgVw.setFitWidth(35);
+        nextInstrImgVw.setFitHeight(35);
+        nextInstrImgVw.setFitWidth(35);
+        skipToEndImgVw.setFitHeight(35);
+        skipToEndImgVw.setFitWidth(35);
+
+        skipToStart.setGraphic(skipToStartImgVw);
+        prevInstr.setGraphic(prevInstrImgVw);
+        currInstr.setGraphic(currInstrImgVw);
+        nextInstr.setGraphic(nextInstrImgVw);
+        skipToEnd.setGraphic(skipToEndImgVw);
+
+        //TODO: Resizing icons/nodes to pane
+        /*
+   skipToStartImgVw.fitHeightProperty().bind(skipToStart.heightProperty());
+   skipToStartImgVw.fitWidthProperty().bind(skipToStart.widthProperty());
+   prevInstrImgVw.fitHeightProperty().bind(prevInstr.heightProperty());
+   prevInstrImgVw.fitWidthProperty().bind(prevInstr.widthProperty());
+   currInstrImgVw.fitHeightProperty().bind(currInstr.heightProperty());
+   currInstrImgVw.fitWidthProperty().bind(currInstr.widthProperty());
+   nextInstrImgVw.fitHeightProperty().bind(nextInstr.heightProperty());
+   nextInstrImgVw.fitWidthProperty().bind(nextInstr.widthProperty());
+   skipToEndImgVw.fitHeightProperty().bind(skipToEnd.heightProperty());
+   skipToEndImgVw.fitWidthProperty().bind(skipToEnd.widthProperty());
+    
+    skipToStart.setMinSize(buttonHBox.getPrefWidth(), buttonHBox.getPrefHeight());
+    skipToStart.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    prevInstr.setMinSize(buttonHBox.getPrefWidth(), buttonHBox.getPrefHeight());
+    prevInstr.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    currInstr.setMinSize(buttonHBox.getPrefWidth(), buttonHBox.getPrefHeight());
+    currInstr.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    nextInstr.setMinSize(buttonHBox.getPrefWidth(), buttonHBox.getPrefHeight());
+    nextInstr.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+    skipToEnd.setMinSize(buttonHBox.getPrefWidth(), buttonHBox.getPrefHeight());
+    skipToEnd.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+         */
+        beginning.setOnAction((event) -> {
+            System.out.println("Option 1 selected");
+        });
+
+        current.setOnAction((event) -> {
+            System.out.println("Option 2 selected");
+        });
+
+        //Highlighting selected instruction is newly added item has an index of N
+        //instrList.getSelectionModel().getSelectedItem();
+        //instrList.getFocusModel().focus(N);
+        //Highlighting scrolled then selected
+        //instrList.scrollTo(N);
+        Platform.runLater(new Runnable() {
+
+            @Override
+            public void run() {
+                // instrList.scrollTo(N);
+                // instrList.getSelectionModel().select(N);
+            }
+        });
+
     }
 }
