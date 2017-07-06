2744f61ba59a1d064e4bd78e96f2079d8a528b32
Sat Garcia <sat@sandiego.edu>
2017-07-06 14:07:18 -0700

    minor code cleanup, mostly just relating to comments and variable names

--- src/reversey/backy/X86Parser.java
+++ src/reversey/backy/X86Parser.java
@@ -13,20 +13,28 @@ import javafx.util.Pair;
  */
 public class X86Parser {
 
+	// Regular expressions used for parsing registers
     private static final String qRegNames = "r(ax|bx|cx|dx|si|di|bp|sp|8|9|1[0-5])";
     private static final String lRegNames = "e(ax|bx|cx|dx|si|di|bp|sp)|r(8|9|1[0-5])d";
     private static final String wRegNames = "(ax|bx|cx|dx|si|di|bp|sp)|r(8|9|1[0-5])w";
     private static final String bRegNames = "(al|ah|bl|bh|cl|ch|dl|dh|sil|dil|bpl|spl)|r(8|9|1[0-5])b";
     private static final String allRegNames = "(" + qRegNames + "|" + lRegNames + "|" + wRegNames + "|" + bRegNames + ")";
 
+	// Regular expressions used for parsing operands
     private static final String constOpRegEx = "\\$(?<const>-?\\p{Digit}+)";
     private static final String regOpRegEx = "\\%(?<regName>\\p{Alnum}+)";
     private static final String memOpRegEx = "(?<imm>-?\\p{Digit}+)?\\s*(?!\\(\\s*\\))\\(\\s*(%(?<base>\\p{Alnum}+))?\\s*(,\\s*%(?<index>\\p{Alnum}+)\\s*(,\\s*(?<scale>\\p{Digit}+))?)?\\s*\\)";
     private static final String labelOpEx = "(?<label>\\w+)";
     private static final String operandRegEx = "\\s*(?<operand>" + constOpRegEx + "|" + regOpRegEx + "|" + memOpRegEx + "|" + labelOpEx + ")\\s*";
 
-    
-    private static int currLineNum = 0;
+    /** 
+	 * The line number that will be given to the next parsed line.
+	 */ 
+    private static int currLineNum = 0; 
+
+	/**
+	 * Map for keeping track of all the labels we have parsed so far.
+	 */
     private static Map<String, Label> labels = new HashMap<String, Label>();
 
     /**
@@ -35,20 +43,34 @@ public class X86Parser {
      * @param instrName The name of the instruction (e.g. addl)
      * @return A pair containing both the instruction type and op size unary
      * instruction.
+	 * @throws X86ParsingException If it is not a valid instruction or if the
+	 * size suffix is invalid.
      */
     public static Pair<InstructionType, OpSize> parseTypeAndSize(String instrName) throws X86ParsingException {
         InstructionType type;
         OpSize size;
 
-        String validTypedInstrNames = "(?<name>add|sub|xor|or|and|shl|sal|shr|sar|mov|lea|inc|dec|neg|not|push|pop|cmp|test)(?<size>b|w|l|q)";
-        String validSetInstrNames = "(?<name>set|j)(?<op>e|ne|s|ns|g|ge|l|le)";
-        
-        Matcher typedMatcher = Pattern.compile(validTypedInstrNames).matcher(instrName);
-        Matcher setMatcher = Pattern.compile(validSetInstrNames).matcher(instrName);
+		/* 
+		 * "sized" instructions are those that have an instruction name (e.g.
+		 * "add") followed by a single character suffix to indicate the size
+		 * (e.g. "q").
+		 */
+        String validSizedInstrNames = "(?<name>add|sub|xor|or|and|shl|sal|shr|sar|mov|lea|inc|dec|neg|not|push|pop|cmp|test)(?<size>b|w|l|q)";
+        Matcher sizedInstrMatcher = Pattern.compile(validSizedInstrNames).matcher(instrName);
+
+		/*
+		 * "conditional" instructions are those whose operations are determined
+		 * by the status flags (e.g. the overflow flag).
+		 * Their suffix isn't a size, rather it is the condition to check for
+		 * (e.g. "ge" for "greater than or equal")
+		 * The "size" of these instructions is implicit (e.g. byte for SET).
+		 */
+        String validConditionalInstrName = "(?<name>set|j)(?<op>e|ne|s|ns|g|ge|l|le)";
+        Matcher condInstrMatcher = Pattern.compile(validConditionalInstrName).matcher(instrName);
             
-        if (typedMatcher.matches()) {
-            type = InstructionType.valueOf(typedMatcher.group("name").toUpperCase());
-            switch (typedMatcher.group("size")) {
+        if (sizedInstrMatcher.matches()) {
+            type = InstructionType.valueOf(sizedInstrMatcher.group("name").toUpperCase());
+            switch (sizedInstrMatcher.group("size")) {
                 case "b":
                     size = OpSize.BYTE;
                     break;
@@ -62,11 +84,14 @@ public class X86Parser {
                     size = OpSize.QUAD;
                     break;
                 default:
-                    throw new X86ParsingException("unexpected size suffix", typedMatcher.start("size"), instrName.length());
+					throw new X86ParsingException("unexpected size suffix",
+													sizedInstrMatcher.start("size"),
+													instrName.length());
             }
-        } else if (setMatcher.matches()) {
+        } else if (condInstrMatcher.matches()) {
             // The SET instruction is implicitly BYTE sized.
-            System.out.println("instrName: "+ instrName);
+			// The JUMP instructions don't really have a size so BYTE is
+			// arbitrarily chosen.
             type = InstructionType.valueOf(instrName.toUpperCase());
             size = OpSize.BYTE;
         } else {
@@ -81,6 +106,7 @@ public class X86Parser {
      *
      * @param name The register's name
      * @return The size of the register.
+	 * @throws X86ParsingException The string did not contain a valid register name.
      */
     public static OpSize getRegisterSize(String name) throws X86ParsingException {
         OpSize opSize = OpSize.BYTE;
@@ -102,8 +128,10 @@ public class X86Parser {
     /**
      * Construct an operand based on a given string.
      *
-     * @param String that contains the operand at the beginning.
+     * @param str String containing the operand at the beginning.
+	 * @param instrOpSize The size of the instruction that will use this * operand.
      * @return The parsed operand.
+	 * @throws X86ParsingException There was an error parsing the string.
      */
     public static Operand parseOperand(String str, OpSize instrOpSize) throws X86ParsingException {
         Operand op = null;
@@ -114,12 +142,10 @@ public class X86Parser {
         Matcher labelMatcher = Pattern.compile(labelOpEx).matcher(str);
 
         if (constMatcher.matches()) {
-            // constant operand
+            // Found a constant operand
             op = new ConstantOperand(Integer.parseInt(constMatcher.group("const"))); // TODO: handle hex
         } else if (regMatcher.matches()) {
-            // register operand
-            String[] splits = str.split(",");
-
+            // Found a register operand
             String regName = regMatcher.group("regName");
 
             OpSize opSize = null;
@@ -131,23 +157,31 @@ public class X86Parser {
                         regMatcher.start("regName") + e.getEndIndex());
             }
 
-            // TODO: move this to a "validation" method
+			// Make sure the size of this register doesn't conflict with the
+			// size the instruction uses/wants.
+            // TODO: move this check to a "validation" method
             if (opSize != instrOpSize) {
                 throw new X86ParsingException("op size mismatch", regMatcher.start("regName"), regMatcher.end("regName"));
             }
 
             op = new RegOperand(regName, opSize);
         } else if (memMatcher.matches()) {
-            // memory operand
+            // Found a memory operand
 
             // All components (e.g. offset or base reg) are optional, although
             // at least one of them must be set.
+
+			// Note that our regular expression should eliminate the possiblity
+			// of getting a memory operand with no components present.
+
+			// Look for an offset, which can be any integral value
             int offset = 0;
             String offsetStr = memMatcher.group("imm");
             if (offsetStr != null) {
                 offset = Integer.parseInt(offsetStr); // TODO: handle hex
             }
 
+			// Look for a base register, which should be a quad sized register
             String baseReg = memMatcher.group("base");
             if (baseReg != null) {
                 OpSize baseOpSize = null;
@@ -165,6 +199,7 @@ public class X86Parser {
                 }
             }
 
+			// Look for an index register, which should be a quad sized register
             String indexReg = memMatcher.group("index");
             if (indexReg != null) {
                 OpSize indexOpSize = null;
@@ -183,6 +218,7 @@ public class X86Parser {
                 }
             }
 
+			// Look for a scaling factor, which should be 1, 2, 4, or 8
             int scale = 1;
             String scaleStr = memMatcher.group("scale");
             if (scaleStr != null) {
@@ -196,18 +232,29 @@ public class X86Parser {
 
             op = new MemoryOperand(baseReg, indexReg, scale, offset, instrOpSize);
         } else if (labelMatcher.matches()) {
+			// Found a label operand
             String labelName = labelMatcher.group("label");
-                op = new LabelOperand(labelName, labels.get(labelName));
-                System.out.println("Found a label operand");
-            // Instruction validation - allow instructions with labels that have not been created yet
+            op = new LabelOperand(labelName, labels.get(labelName));
+            // TODO: Instruction validation - allow instructions with labels that have not been created yet
             // What happens when instruction with jmp label is executed but jumps to a label that hasn't been created yet?
-    } else {
-            System.out.println("Unknown type of operand");
-            System.out.println("Tried to match " + str);
+        } else {
+			// TODO: throw X86ParsingException here
+            System.out.println("ERROR: Unknown type of operand.");
+            System.out.println("\t Tried to match " + str);
+			System.exit(1);
         }
         return op;
     }
 
+	/**
+	 * Parse all the operands in the given string.
+	 * These operands should be comma separated.
+	 *
+	 * @param operandsString The string to parse for operands.
+	 * @param opSize The expected size of the operand.
+	 * @return The list of operands that were parsed.
+	 * @throws X86ParsingException There was a problem parsing the operands.
+	 */
     public static List<Operand> parseOperands(String operandsStr, OpSize opSize) throws X86ParsingException {
         List<Operand> operands = new ArrayList<Operand>();
 
@@ -218,14 +265,18 @@ public class X86Parser {
 
         int nextIndex = -1;
         try {
+			// Parse the first operand
             String opStr = m.group("operand");
             Operand op = parseOperand(opStr, opSize);
             nextIndex = m.end();
 
             operands.add(op);
 
+			// Update pattern to include the comma separator for the following
+			// operands
             m = Pattern.compile("," + operandRegEx).matcher(operandsStr);
 
+			// Keep parsing operands until we don't find any more
             while (m.find(nextIndex)) {
                 opStr = m.group("operand");
                 op = parseOperand(opStr, opSize);
@@ -238,6 +289,7 @@ public class X86Parser {
                     m.start("operand") + e.getEndIndex());
         }
 
+		// Make sure there isn't any leftover cruft after the last parsed operand
         if (nextIndex != operandsStr.length()) {
             throw new X86ParsingException("unexpected value", nextIndex, operandsStr.length());
         }
@@ -255,11 +307,17 @@ public class X86Parser {
         Matcher instMatcher = Pattern.compile("\\s*(?<inst>\\S+)\\s+(?<operands>.*)").matcher(instr);
         Matcher labelMatcher = Pattern.compile("\\s*(?<label>\\w+):\\s*").matcher(instr);
 
+		// The line should be either a label or an instruction
         if (!instMatcher.matches() && !labelMatcher.matches()) {
             throw new X86ParsingException("nonsense input", 0, instr.length());
         }
 
         if (instMatcher.matches()) {
+			// This line contains an X86 instruction.
+			
+			// Step 1: Get the name of the instruction and use that to determine
+			// the type (i.e. what operation it is performing) and size of the
+			// instruction.
             String instrName = instMatcher.group("inst");
 
             Pair<InstructionType, OpSize> instDetails = null;
@@ -274,6 +332,9 @@ public class X86Parser {
             InstructionType instrType = instDetails.getKey();
             OpSize opSize = instDetails.getValue();
 
+			// Step 2: Parse the operands (putting them into a list) then use
+			// those operands plus the instruction type to create a new
+			// X86Instruction.
             String operandsStr = instMatcher.group("operands");
 
             List<Operand> operands = null;
@@ -286,17 +347,20 @@ public class X86Parser {
             }
 
             if (operands.size() != instrType.numOperands()) {
-                System.out.println("Num operands: " + operands.size());
-                throw new X86ParsingException("too many operands",
+                throw new X86ParsingException("wrong number of operands",
                         instMatcher.start("operands"),
                         instr.length());
             } else if (instrType.numOperands() == 2) {
+				// This is a binary instruction (e.g. add), then second operand
+				// should be something we can write to (i.e. not a constant or a
+				// label)
                 if (operands.get(1) instanceof ConstantOperand) {
                     // FIXME: start/end index is not right here
                     throw new X86ParsingException("destination cannot be a constant",
                             instMatcher.start("operands"),
                             instr.length());
                 }
+				// TODO: check that the destination isn't a LabelOperand either
                 return new x86BinaryInstruction(instrName.substring(0, instrName.length() - 1),
                         operands.get(0),
                         operands.get(1),
@@ -307,24 +371,33 @@ public class X86Parser {
                     instrName = instrName.substring(0, instrName.length() - 1);
                 }
 
-                // TODO: throw exception if destination is a constant
+                // TODO: throw exception if destination is a constant (or a
+				// label for non-jump instructions)
                 return new x86UnaryInstruction(instrName,
                         operands.get(0),
                         opSize,
                         currLineNum++);
             } else {
-                throw new X86ParsingException("unsupported instruction type", 0, instrName.length());
+				throw new X86ParsingException("unsupported instruction type", 0,
+												instrName.length());
             }
         } else {
+			// This line contains a label
             String labelName = labelMatcher.group("label");
+
+			// Make sure this label doesn't already exist
             if (labels.containsKey(labelName)) {
                 System.out.println("Duplicate label");
                 System.exit(1);
+				throw new X86ParsingException("Duplicate label name", 
+												labelMatcher.start("label"),
+												labelMatcher.end("label"));
             }
 
             Label l = new Label(labelName, currLineNum++);
             labels.put(labelName, l);
             return l;
         }
+		// TODO: allow lines that contain both a label and an instruction?
     }
 }
