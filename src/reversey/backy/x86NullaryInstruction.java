be6e45e9614bbc6657261fe1156998f32c8002fd
Sat Garcia <sat@sandiego.edu>
2017-08-03 15:38:15 -0700

    bug fix relating to updated MemoryOperand constructor

--- src/reversey/backy/x86NullaryInstruction.java
+++ src/reversey/backy/x86NullaryInstruction.java
@@ -53,7 +53,7 @@ public class x86NullaryInstruction extends x86Instruction {
         Map<String, Boolean> flags = new HashMap<>();
         
         // step 1: store (%rsp) value in rip register 
-        MemoryOperand src = new MemoryOperand("rsp", null, 1, 0, this.opSize);
+        MemoryOperand src = new MemoryOperand("rsp", null, null, null, this.opSize);
         MachineState tmp = state.cloneWithNewRIP(src.getValue(state).intValue());
 
         // step 2: add 8 to rsp
